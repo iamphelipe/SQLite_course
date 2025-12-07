3c7383ba93723844fd08cb96d321c00cfff12e21
iamphelipe <phelipeguimaraes2023@gmail.com>
2025-12-06 23:55:35 -0300

    cascade, on update, foreign key

--- pratica5curso/comandos.sql
+++ pratica5curso/comandos.sql
@@ -1,103 +1,32 @@
 --database: ./db.sqlite
 
-CREATE TABLE "cursos" (id, nome, aulas);
-
-DROP TABLE cursos;
-
 CREATE TABLE "cursos" (
-    id INTEGER NOT NULL, 
-    nome TEXT NOT NULL,
-    aulas INTEGER
-);
+    "id" INTEGER PRIMARY KEY,
+    "nome" TEXT
+) STRICT;
 
-PRAGMA table_info("cursos");
+CREATE TABLE "aulas" (
+    "id" INTEGER PRIMARY KEY,
+    "curso_id" INTEGER,
+    "nome" TEXT,
+    FOREIGN KEY("curso_id") REFERENCES "cursos" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+) STRICT;
 
+INSERT INTO "cursos" ("nome")
+    VALUES ('HTML');
 
-SELECT * from cursos;
+INSERT INTO "cursos" ("nome")
+    VALUES ('CSS');
 
-INSERT INTO "cursos" (id, nome, aulas)
-    VALUES (1, 'HTML', 20),
-           (2, 'CSS', 30),
-           (3, 'JS', 17);
+INSERT INTO "cursos" ("nome")
+    VALUES ('JS');
 
-INSERT INTO cursos(id, nome, aulas) VALUES (
-    4, 'React', 15
-);
+INSERT INTO "aulas" ("curso_id", "nome")
+    VALUES (1, 'Fundamentos do HTML');
 
-INSERT INTO "cursos" (id, nome, aulas) VALUES ('HTML', 20);
+DELETE FROM "cursos" WHERE "id" = 1;
 
-
-DELETE FROM cursos;
-DELETE FROM "cursos" WHERE "nome" = 'JS';
-
-
-SELECT id, nome FROM cursos LIMIT 2;
-
-SELECT id, nome FROM cursos WHERE id > 2; 
-SELECT id, nome FROM cursos WHERE id > 2 AND aulas > 10;
-
-UPDATE "cursos" SET "nome" = 'HTML e CSS',  aulas = 11 WHERE "id" = 1;
+UPDATE "cursos" SET ("id") = 1 WHERE id = 3;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Exercicio 01
-
-CREATE TABLE "produtos" (
-    "id" INTEGER NOT NULL,
-    "nome" TEXT NOT NULL,
-    "preco" INTEGER NOT NULL
-    );
-CREATE TABLE "clientes" (
-    "id" INTEGER NOT NULL, 
-    "nome" TEXT NOT NULL, 
-    "email" TEXT NOT NULL
-    );
-CREATE TABLE "compras" (
-    "id" INTEGER NOT NULL,
-    "cliente_id" INTEGER NOT NULL, 
-    "produto_id" INTEGER NOT NULL, 
-    "data" TEXT NOT NULL
-    );
-
-INSERT INTO "produtos"
-    VALUES (
-        1, 'Notebook', 1000
-    ), (
-        2, 'Smartphone', 500
-    ), (
-        3, 'Tablet', 300
-    );
-
-INSERT INTO "clientes"
-    VALUES (
-    1, 'Maria', 'maria@email.com'
-    ), (
-        2, 'Joao', 'joao@email.com'
-    );
-
-INSERT INTO "compras"
-    VALUES (
-        1, 2, 1, '2049-01-01'
-    ), (
-        2, 1, 2, '2049-01-02'
-    ), (
-        3, 2, 3, '2049-01-03'
-    );
-
-SELECT * FROM "produtos";
-
-SELECT "nome" FROM "produtos" WHERE "preco" > 400;
-
-SELECT id, "cliente_id" AS "Joao", "produto_id", "data" FROM "compras" WHERE cliente_id = 2;
+DROP TABLE aulas
